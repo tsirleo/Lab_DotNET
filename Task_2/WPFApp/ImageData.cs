70fefabdf47e3e9ee810d98848bf88ff487919b6
tsirleo <lion.tsirunoff@yandex.ru>
2022-10-26 01:52:30 +0300

    second task files added

---
+++ Task_2/WPFApp/ImageData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace WPFApp
+{
+    public class ImageData
+    {
+        public string fileName { get; set; }
+        public string imgPath { get; set; }
+        public Dictionary<string, double> emotionsDict { get; set; } 
+
+        public ImageData(string name, string path, Dictionary<string, double> dict)
+        {
+            fileName = name;
+            imgPath = path;
+            emotionsDict = new Dictionary<string, double>(dict);
+        }
+    }
+}
