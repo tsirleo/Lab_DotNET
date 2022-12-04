14bb9ca774fd8995e5c18b1978dfae599af80334
tsirleo <lion.tsirunoff@yandex.ru>
2022-12-04 22:05:18 +0300

    unused import deleted

--- Task_4/Server/Controllers/ServerControllers.cs
+++ Task_4/Server/Controllers/ServerControllers.cs
@@ -1,11 +1,9 @@
 using System.Collections.ObjectModel;
 using System.Net;
-using System.Threading.Tasks;
 using Contracts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Server.Database;
-using static System.Net.WebRequestMethods;
 
 namespace Server.Controllers
 {
