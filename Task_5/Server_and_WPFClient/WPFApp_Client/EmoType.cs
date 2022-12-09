d6151f7052eec865f0cc2fb6f82f9d29b0324e97
tsirleo <lion.tsirunoff@yandex.ru>
2022-12-10 01:17:22 +0300

    WPF client added

---
+++ Task_5/Server_and_WPFClient/WPFApp_Client/EmoType.cs
@@ -0,0 +1,14 @@
+﻿namespace WPFApp_Client
+{
+    public enum EmoType
+    {
+        anger,
+        contempt,
+        disgust,
+        fear,
+        happiness,
+        neutral,
+        sadness,
+        surprise
+    }
+}
